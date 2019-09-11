ed6090a79a9469d57b9d8e34e39267583ccc7dfd
Darren SooHoo <darsoohoo@gmail.com>
2019-09-10 22:17:54 -0700

    update

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    // check if both strings are same length not including punctuations
 
+    const checker = "abcdefghijklmnopqrstuvwxyz123456789"
+    let aArray = []
+    let bArray = []
+    for(let i = 0; i < stringA.length; i++) {
+        if (checker.includes(stringA[i])) {
+            aArray.push(stringA[i])
+        }
+    }
+   
+
+    for(let i = 0; i < stringB.length; i++) {
+        if (checker.includes(stringB[i])) {
+            bArray.push(stringB[i])
+        }
+    }
+    
+    let newStringA = aArray.sort().join('')
+    let newStringB = bArray.sort().join('')
+
+    // check if each letter appears the same amount of times
+    if (newStringA === newStringB) {
+    
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+const strA = 'rail safety'
+const strB = 'fairy tales'
+anagrams(strA,strB)
 module.exports = anagrams;
